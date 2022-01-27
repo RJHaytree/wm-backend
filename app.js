0c389c4c470a1365d398fdebd799bc2c97959ee5
RJHaytree <rjhaytree@gmail.com>
2022-01-27 22:03:57 +0000

    Update CORS

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(logger('dev'));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', 'https://orange-dune-069981603.1.azurestaticapps.net/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Locale, Content-Type,Origin,Accept, X-Requested-With, Access-Control-Request-Method,Access-Control-Request-Headers');
     res.setHeader('Access-Control-Allow-Credentials', true);
